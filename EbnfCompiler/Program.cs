98942246659381d5908781dc8b58986a29f67f2d
Rick Dodd <rick.dodd@comcast.net>
2020-05-02 10:53:58 -0500

    formatting

--- EbnfCompiler/Program.cs
+++ EbnfCompiler/Program.cs
@@ -26,14 +26,17 @@ namespace EbnfCompiler
 
          var scanner = new Scanner.Scanner(stream);
 
-         var astBuilder = new AstBuilder(new AstNodeFactory(tracer), new ProdInfoFactory(tracer), new Stack<IAstNode>(), tracer);
+         var astBuilder = new AstBuilder(new AstNodeFactory(tracer), 
+                                         new ProdInfoFactory(tracer), 
+                                         new Stack<IAstNode>(), tracer);
 
          var parser = new Parser.Parser(scanner, astBuilder);
 
          var (tokens, ast) = parser.ParseGoal();
 
          var traverser = new AstTraverser(tracer);
-         var gen = new CSharpGenerator(ast, tokens, traverser, loggerFactory.CreateLogger("CSGEN"));
+         var gen = new CSharpGenerator(ast, tokens, traverser, 
+                                   loggerFactory.CreateLogger("CSGEN"));
          gen.Run();
       }
    }
